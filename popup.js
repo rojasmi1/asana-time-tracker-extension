117410a3c859d785cccbd5b3eeb12404c95b7732
Michael Rojas <michael.rojasp@gmail.com>
2016-12-05 10:43:20 -0600

    Extract task id from the tab URL

--- popup.js
+++ popup.js
@@ -3,10 +3,13 @@ $(function(){
 $('#trackTask').click(function(){
 
 chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-  var currentURL = tabs[0].url;
+
+  //The task id are the digits after the last slash
+  var taskId = tabs[0].url.substr(tabs[0].url.lastIndexOf('/')+1);
+
   //Insert entry in mongo using REST API
   $.ajax( { url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?apiKey=omy9HzMATiLB1SlUDJfkjvB-Q1LVV0tj",
-  		  data: JSON.stringify({ "task_id" : currentURL,"duration":$('#duration').val()}),
+  		  data: JSON.stringify({ "task_id" : taskId,"duration":$('#duration').val()}),
   		  type: "POST",
   		  contentType: "application/json" } );
     });
