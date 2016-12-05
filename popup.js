089bf3555bccb16ddb3e04f101e45d107b999096
Michael Rojas <michael.rojasp@gmail.com>
2016-12-05 12:32:01 -0600

    Show notification after tracking a task.

--- popup.js
+++ popup.js
@@ -1,18 +1,25 @@
 $(function(){
 
 $('#trackTask').click(function(){
-
 chrome.tabs.query({active:true,currentWindow:true},function(tabs){
 
   //The task id are the digits after the last slash
   var taskId = tabs[0].url.substr(tabs[0].url.lastIndexOf('/')+1);
 
   //Insert entry in mongo using REST API
-  $.ajax( { url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?apiKey=omy9HzMATiLB1SlUDJfkjvB-Q1LVV0tj",
+  $.ajax({ url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?apiKey=omy9HzMATiLB1SlUDJfkjvB-Q1LVV0tj",
   		  data: JSON.stringify({ "task_id" : taskId,"duration":$('#duration').val()}),
   		  type: "POST",
-  		  contentType: "application/json" } );
+  		  contentType: "application/json" }).then(function(data){
+          $('#duration').val('');
+          var notificationOptions = {type:"basic",title:"Task Tracked",message:"The task has been successfully tracked.",iconUrl:"asana-exporter-icon.png"};
+          chrome.notifications.create('tasktracked',notificationOptions,function(){
+            window.close();
+          });
+        });
     });
+
+
 });
 
 });
