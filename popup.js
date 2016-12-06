9cd9c1eec256b2b4f160b179e158ff9d55d24b28
Michael Rojas <michael.rojasp@gmail.com>
2016-12-06 15:03:25 -0600

    Enrties can be updated if they already exist in the database.

--- popup.js
+++ popup.js
@@ -1,4 +1,15 @@
 $(function(){
+    //Check for the current task in the database, if it already exists then the current duration is shown
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+    //The task id are the digits after the last slash
+    var taskId = tabs[0].url.substr(tabs[0].url.lastIndexOf('/')+1);
+    $.ajax({ url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?q={\"task_id\":\""+taskId+"\"}&apiKey=omy9HzMATiLB1SlUDJfkjvB-Q1LVV0tj",
+          type: "GET"}).then(function(tasks){
+            console.log(tasks[0]);
+            $('#duration').val(tasks[0].duration)
+          })
+    });
+
 
 $('#trackTask').click(function(){
 chrome.tabs.query({active:true,currentWindow:true},function(tabs){
@@ -7,9 +18,9 @@ chrome.tabs.query({active:true,currentWindow:true},function(tabs){
   var taskId = tabs[0].url.substr(tabs[0].url.lastIndexOf('/')+1);
 
   //Insert entry in mongo using REST API
-  $.ajax({ url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?apiKey=omy9HzMATiLB1SlUDJfkjvB-Q1LVV0tj",
-  		  data: JSON.stringify({ "task_id" : taskId,"duration":$('#duration').val()}),
-  		  type: "POST",
+  $.ajax({ url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?u=true&q={\"task_id\":\""+ taskId +"\"}&apiKey=omy9HzMATiLB1SlUDJfkjvB-Q1LVV0tj",
+  		  data: JSON.stringify({ "$set":{"task_id" : taskId,"duration":$('#duration').val()}}),
+  		  type: "PUT",
   		  contentType: "application/json" }).then(function(data){
           $('#duration').val('');
           var notificationOptions = {type:"basic",title:"Task Tracked",message:"The task has been successfully tracked.",iconUrl:"asana-exporter-icon.png"};
@@ -18,7 +29,6 @@ chrome.tabs.query({active:true,currentWindow:true},function(tabs){
           });
         });
     });
-
 
 });
 
