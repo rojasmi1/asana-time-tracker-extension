5879a5a9477cfc01e9c5fa660fe5f2bcc9ed82c0
Michael Rojas <michael.rojasp@gmail.com>
2016-12-12 15:14:37 -0600

    Read mongolab API key from Chrome Storage.

--- popup.js
+++ popup.js
@@ -1,11 +1,12 @@
 $(function(){
     //Check for the current task in the database, if it already exists then the current duration is shown
-  chrome.storage.sync.get("apiKey",function(items){
-    var apiKey = items.apiKey;
+  chrome.storage.sync.get(["asanaApiKey","mongolabApiKey"],function(items){
+    var asanaApiKey = items.asanaApiKey;
+    var mongolabApiKey = items.mongolabApiKey;
     chrome.tabs.query({active:true,currentWindow:true},function(tabs){
     //The task id are the digits after the last slash
     var taskId = tabs[0].url.substr(tabs[0].url.lastIndexOf('/')+1);
-    $.ajax({ url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?q={\"taskId\":\""+taskId+"\",\"asanaApiKey\":\""+apiKey+"\"}&apiKey=omy9HzMATiLB1SlUDJfkjvB-Q1LVV0tj",
+    $.ajax({ url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?q={\"taskId\":\""+taskId+"\",\"asanaApiKey\":\""+asanaApiKey+"\"}&apiKey="+mongolabApiKey,
           type: "GET"}).then(function(tasks){
             console.log(tasks[0]);
             $('#duration').val(tasks[0].duration)
@@ -15,17 +16,18 @@ $(function(){
 
 
 $('#trackTask').click(function(){
-  chrome.storage.sync.get(["apiKey","fullName"],function(items){
-    var apiKey = items.apiKey;
+  chrome.storage.sync.get(["asanaApiKey","fullName","mongolabApiKey"],function(items){
+    var asanaApiKey = items.asanaApiKey;
     var fullName = items.fullName;
+    var mongolabApiKey = items.mongolabApiKey;
     chrome.tabs.query({active:true,currentWindow:true},function(tabs){
     
       //The task id are the digits after the last slash
       var taskId = tabs[0].url.substr(tabs[0].url.lastIndexOf('/')+1);
     
       //Insert entry in mongo using REST API
-      $.ajax({ url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?u=true&q={\"taskId\":\""+ taskId +"\",\"asanaApiKey\":\""+apiKey+"\"}&apiKey=omy9HzMATiLB1SlUDJfkjvB-Q1LVV0tj",
-      		  data: JSON.stringify({ "$set":{"taskId" : taskId,"asanaApiKey":apiKey,"fullName":fullName,"duration":$('#duration').val()}}),
+      $.ajax({ url: "https://api.mlab.com/api/1/databases/asana-time-tracker/collections/task-entry?u=true&q={\"taskId\":\""+ taskId +"\",\"asanaApiKey\":\""+asanaApiKey+"\"}&apiKey="+mongolabApiKey,
+      		  data: JSON.stringify({ "$set":{"taskId" : taskId,"asanaApiKey":asanaApiKey,"fullName":fullName,"duration":$('#duration').val()}}),
       		  type: "PUT",
       		  contentType: "application/json" }).then(function(data){
               $('#duration').val('');
