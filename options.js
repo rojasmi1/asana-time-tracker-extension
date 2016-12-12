a4b97a27181f21f7ad9712b90084b9095cc1573c
Michael Rojas <michael.rojasp@gmail.com>
2016-12-12 15:07:02 -0600

    Add mongolab API key as part of the options for the extension.

--- options.js
+++ options.js
@@ -1,11 +1,12 @@
 $(function(){
-  chrome.storage.sync.get(["apiKey","fullName"],function(items){
-    $('#apiKey').val(items.apiKey);
+  chrome.storage.sync.get(["asanaApiKey","mongolabApiKey","fullName"],function(items){
+    $('#asanaApiKey').val(items.asanaApiKey);
     $('#fullName').val(items.fullName);
+    $('#mongolabApiKey').val(items.mongolabApiKey);
   });
 
   $('#updateOptions').click(function(){
-    chrome.storage.sync.set({"apiKey":$('#apiKey').val(),"fullName":$('#fullName').val()},function(){
+    chrome.storage.sync.set({"asanaApiKey":$('#asanaApiKey').val(),"fullName":$('#fullName').val(),"mongolabApiKey":$('#mongolabApiKey').val()},function(){
       var notificationOptions = {type:"basic",title:"Options saved",message:"Options successfully saved.",iconUrl:"asana-exporter-icon.png"};
       chrome.notifications.create('optionsSaved',notificationOptions,function(){});
     });
